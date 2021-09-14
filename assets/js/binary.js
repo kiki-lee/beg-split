4a0bc05971dcfb83b192d9d23074f184d3510b64
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2021-09-13 17:16:59 -0700

    0.0.100

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.5.56/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.99","target":"arcade","targetVersion":"1.5.56","repo":"kiki-lee/beg-split"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.5.56/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.100","target":"arcade","targetVersion":"1.5.56","repo":"kiki-lee/beg-split"}
 // total=1150275 new=5.58% cached=88.90% other=5.52%
 (function (ectx) {
 'use strict';
@@ -385,7 +385,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P82203(s) {
+function _main___P82420(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -750,13 +750,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P82203.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P82203.continuations = [  ]
+_main___P82420.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P82420.continuations = [  ]
 
-function _main___P82203_mk(s) {
+function _main___P82420_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P82203, depth: s.depth + 1,
+        parent: s, fn: _main___P82420, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -38969,5 +38969,5 @@ const sprites_StaticObstacle__C67685_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["mySprite___69327"])
 
-return _main___P82203
+return _main___P82420
 })
